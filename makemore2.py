dbc72db9e5c0debb192585cee143c04805797ec8
Ali <alimalik16@hotmail.com>
2023-05-14 02:30:40 +0100

    Non-linear, Forward/backward, loss, update, stats

--- makemore2.py
+++ makemore2.py
@@ -5,29 +5,29 @@ import matplotlib.pyplot as plt
 words = open('names.txt', 'r').read().splitlines()
 
 # build the vocabulary of characters and mappings to/from integers
-chars = sorted(list(''.join(words))) 
+chars = sorted(list(set(''.join(words))))
 stoi = {s:i+1 for i,s in enumerate(chars)}
-
 stoi['.'] = 0
-
-itos = {i+1:s for s,i in stoi.items() }
+itos = {i:s for s,i in stoi.items()}
 vocab_size = len(itos)
-#Build the dataset
-def build_dataset(words):
-    X,Y = [],[]
 
-    context_size = 3
-    for w in words:
-        for char in w+'.':
-            ix = stoi[char]
-            context = [0] * context_size
-            X.append(context)
-            Y.append(ix)
-            context = context[1:] + [ix]
-    
-    X = torch.tensor(X)
-    Y = torch.tensor(Y)
-    return X,Y
+#Build the dataset
+block_size= 3
+def build_dataset(words):  
+  X, Y = [], []
+  
+  for w in words:
+    context = [0] * block_size
+    for ch in w + '.':
+      ix = stoi[ch]
+      X.append(context)
+      Y.append(ix)
+      context = context[1:] + [ix] # crop and append
+
+  X = torch.tensor(X)
+  Y = torch.tensor(Y)
+  
+  return X, Y
 
 import random
 random.seed(42)
@@ -40,12 +40,11 @@ Xdev, Ydev = build_dataset(words[n1:n2])   # 10%
 Xte,  Yte  = build_dataset(words[n2:])     # 10%
 
 #Preparation for MLP
-block_size = 3 #number of characters to be inputted at once
 n_embd = 10 # dimensionality for character vectors
 n_hidden = 200 # number of neurons in hidden layer
 
 g = torch.Generator().manual_seed(2147483647)
-C = torch.randn(vocab_size,n_embd)
+C  = torch.randn((vocab_size, n_embd),            generator=g)
 W1 = torch.randn((n_embd* block_size, n_hidden),generator=g) * (5/3) / ((n_embd * block_size)**0.5)
 W2 = torch.randn((n_hidden,vocab_size),generator =g) * 0.01
 b2 = torch.randn(vocab_size,generator=g) * 0
@@ -62,15 +61,16 @@ for p in parameters:
 
 #Training
 
-max_steps = 200,000
+max_steps = 200000
 batch_size = 32
 lossi = []
 
 for i in range(max_steps):
 
     #minibatch construct
-    indices = randint(0,Xtr.shape[0],(batch_size,), generator=g)
+    indices = torch.randint(0,Xtr.shape[0],(batch_size,), generator=g)
     Xb, Yb = Xtr[indices],Ytr[indices]
+    
 
     #forward pass
     emb = C[Xb]
@@ -85,6 +85,28 @@ for i in range(max_steps):
     with torch.no_grad():
         bnmean_running = 0.999 * bnmean_running + 0.001 * bnmeani
         bnstd_running = 0.999 * bnstd_running + 0.001 * bnstdi
+    #Non-linearity
+    h = torch.tanh(hpreact)
+    output = h @ W2 + b2 # output layer
+    
+    loss = F.cross_entropy(output,Yb)
+
+    #Backward pass
+    for p in parameters:
+        p.grad = None
+    loss.backward()
+
+    #Update parameters
+    lr = 0.1 if i < 100000 else 0.01
+    for p in parameters:
+        p.data += -lr * p.grad
+    # track stats
+    if i % 10000 == 0: # print every once in a while
+        print(f'{i:7d}/{max_steps:7d}: {loss.item():.4f}')
+    lossi.append(loss.log10().item())
+
+plt.plot(lossi)
+        
 
 
 
